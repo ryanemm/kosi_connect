d27420f3fca72af62aee8fd3603a7a3d73e14557
ryanemm <59361481+ryanemm@users.noreply.github.com>
2021-09-15 20:09:19 +0200

    service section mobile view

--- lib/Sections/Services/service_section_mobile.dart
+++ lib/Sections/Services/service_section_mobile.dart
@@ -1,7 +1,7 @@
 import 'package:animated_text_kit/animated_text_kit.dart';
 import "package:flutter/material.dart";
 import "package:google_fonts/google_fonts.dart";
-import 'package:kosi_connect/Sections/Services/service_card.dart';
+import 'package:kosi_connect/Sections/Services/service_card_mobile.dart';
 import "package:shape_of_view/shape_of_view.dart";
 
 class ServiceSectionMobile extends StatelessWidget {
@@ -9,13 +9,24 @@ class ServiceSectionMobile extends StatelessWidget {
   Widget build(BuildContext context) {
     Size _screenSize = MediaQuery.of(context).size;
     return Container(
-      padding: EdgeInsets.only(left: 30, right: 30, top: 40, bottom: 40),
-      color: Colors.white,
-      child: Column(
+      padding: EdgeInsets.only(bottom: 20),
+      color: Colors.grey[200],
+      child: ShapeOfView(
+        elevation: 6,
+        shape: ArcShape(
+          position: ArcPosition.Bottom,
+          height: 40,
+          direction: ArcDirection.Outside,
+        ),
+        child:
+        Container(
+          color: Colors.white,
+          padding: EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 30),
+          child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          AnimatedTextKit(
+          /*AnimatedTextKit(
             totalRepeatCount: 10,
             animatedTexts: [
               TypewriterAnimatedText("<my services>",
@@ -25,27 +36,22 @@ class ServiceSectionMobile extends StatelessWidget {
                       fontWeight: FontWeight.w600,
                       color: Color.fromARGB(255, 0, 0, 50)))
             ],
-          ),
+          ),*/
           SizedBox(height: 10),
-          Center(
+          /*Center(
             child: Image.asset(
               "assets/images/web_dev_graphic.png",
               height: 160,
             ),
-          ),
-          SizedBox(height: 10),
-          Center(
-            child: Text(
-                "I\'m a software developer specialising building (and designing) beautiful experiences for web and mobile apps.",
-                style: GoogleFonts.nunito(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w500,
-                    color: Colors.grey[800]),
-                textAlign: TextAlign.center,
-                softWrap: true),
-          ),
+          ),*/
+          //SizedBox(height: 10),
+      ServiceCardMobile(index: 0),
+      ServiceCardMobile(index: 1),
+      ServiceCardMobile(index: 2),
         ],
       ),
+    ),
+    ),
     );
   }
 }
