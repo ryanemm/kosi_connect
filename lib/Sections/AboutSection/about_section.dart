d41a64f9037b8020580b8f71c19935b8569f97ec
Ryan Emerson <emersonryant@gmail.com>
2022-01-18 17:26:32 +0200

    about section mobile

--- lib/Sections/AboutSection/about_section.dart
+++ lib/Sections/AboutSection/about_section.dart
@@ -1,7 +1,7 @@
 import "package:flutter/material.dart";
-import 'package:kosi_connect/Sections/AboutSection/Components/about_info_sign.dart';
-import "package:shape_of_view/shape_of_view.dart";
+
 import "package:assorted_layout_widgets/assorted_layout_widgets.dart";
+import 'package:google_fonts/google_fonts.dart';
 
 class AboutSection extends StatelessWidget {
   @override
@@ -9,17 +9,46 @@ class AboutSection extends StatelessWidget {
     Size _screenSize = MediaQuery.of(context).size;
     return Box(
       color: Colors.white,
-      padding: const EdgeInsets.only(bottom: 70),
-      child: ShapeOfView(
-        elevation: 10,
-        height: _screenSize.height * 0.7,
-        shape: DiagonalShape(
-            position: DiagonalPosition.Bottom,
-            direction: DiagonalDirection.Right,
-            angle: DiagonalAngle.deg(angle: 4)),
-        child: Center(
-          child: AboutInfoSign(),
-        ),
+      padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 40),
+      child: Column(
+        children: [
+          Text(
+            "About Us",
+            style: GoogleFonts.novaRound(
+              fontSize: 32,
+              color: Color(0xFF003049),
+            ),
+          ),
+          Container(
+            height: 3,
+            width: 120,
+            color: Colors.amberAccent,
+          ),
+          SizedBox(height: 30),
+          Text(
+            "A web and mobile developer from Johannesburg, South Africa. I create bespoke websites and mobile apps to help bring your business to life.",
+            style: GoogleFonts.nunito(
+              fontSize: 18,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height: 30),
+          Text(
+            "Our Mission",
+            style: GoogleFonts.novaRound(
+              fontSize: 24,
+              color: Color(0xFF003049),
+            ),
+          ),
+          SizedBox(height: 30),
+          Text(
+            "A web and mobile developer from Johannesburg, South Africa. I create bespoke websites and mobile apps to help bring your business to life.",
+            style: GoogleFonts.nunito(
+              fontSize: 18,
+            ),
+            textAlign: TextAlign.center,
+          )
+        ],
       ),
     );
   }
