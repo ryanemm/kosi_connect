de0bea6e0a95d885f0a789259d3e1e4975738567
Ryan Emerson <emersonryant@gmail.com>
2022-02-22 15:01:34 +0200

    emails + skyline image

--- lib/Sections/Contact/Layouts/mobile_contact.dart
+++ lib/Sections/Contact/Layouts/mobile_contact.dart
@@ -24,12 +24,123 @@ class MobileContact extends StatelessWidget {
               fontWeight: FontWeight.w600,
             ),
           ),
+          SizedBox(height: 20),
+          GestureDetector(
+            onTap: () {
+              launch("mailto:ryan@kosiconnect.co.za? subject=Enquiry");
+            },
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Email: ",
+                    style: GoogleFonts.nunito(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w400,
+                        color: Colors.grey[800])),
+                Text("ryan@kosiconnect.co.za",
+                    style: GoogleFonts.nunito(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.grey[800],
+                      decoration: TextDecoration.underline,
+                      decorationColor: Colors.grey[800],
+                      decorationThickness: 2,
+                    )),
+                Icon(
+                  Icons.email_rounded,
+                  color: Color(0xFF003049),
+                  // size: 30,
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 15),
+          GestureDetector(
+            onTap: () {
+              launch("mailto:kayn@kosiconnect.co.za? subject=Enquiry");
+            },
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("kayn@kosiconnect.co.za",
+                    style: GoogleFonts.nunito(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.grey[800],
+                      decoration: TextDecoration.underline,
+                      decorationColor: Colors.grey[800],
+                      decorationThickness: 2,
+                    )),
+                Icon(
+                  Icons.email_rounded,
+                  color: Color(0xFF003049),
+                  // size: 30,
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 15),
+          GestureDetector(
+            onTap: () {
+              launch("tel:+27 73 016 9813");
+            },
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Tel: ",
+                    style: GoogleFonts.nunito(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w400,
+                        color: Colors.grey[800])),
+                SizedBox(height: 10),
+                Text("073 016 9813",
+                    style: GoogleFonts.nunito(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.grey[800],
+                      decoration: TextDecoration.underline,
+                      decorationColor: Colors.grey[800],
+                      decorationThickness: 2,
+                    )),
+                Icon(
+                  Icons.call,
+                  color: Color(0xFF003049),
+                  // size: 30,
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 15),
+          GestureDetector(
+            onTap: () {
+              launch("tel:+27 67 989 0637");
+            },
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("067 989 0637",
+                    style: GoogleFonts.nunito(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.grey[800],
+                      decoration: TextDecoration.underline,
+                      decorationColor: Colors.grey[800],
+                      decorationThickness: 2,
+                    )),
+                Icon(
+                  Icons.call,
+                  color: Color(0xFF003049),
+                  // size: 30,
+                ),
+              ],
+            ),
+          ),
           Image.asset("assets/images/skyline_grey.png"),
           Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              SizedBox(height: 20),
+              SizedBox(height: 40),
               Text(
                 "372 Long Homes, Umjindi,",
                 style: GoogleFonts.nunito(
@@ -54,66 +165,9 @@ class MobileContact extends StatelessWidget {
                     color: Colors.grey[800]),
               ),
               SizedBox(height: 10),
-              GestureDetector(
-                onTap: () {
-                  launch("tel:+27 67 989 0637");
-                },
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text("Tel: ",
-                        style: GoogleFonts.nunito(
-                            fontSize: 18,
-                            fontWeight: FontWeight.w400,
-                            color: Colors.grey[800])),
-                    Text("067 989 0637",
-                        style: GoogleFonts.nunito(
-                          fontSize: 18,
-                          fontWeight: FontWeight.w400,
-                          color: Colors.grey[800],
-                          decoration: TextDecoration.underline,
-                          decorationColor: Colors.grey[800],
-                          decorationThickness: 2,
-                        )),
-                    Icon(
-                      Icons.call,
-                      color: Color(0xFF003049),
-                      // size: 30,
-                    ),
-                  ],
-                ),
-              ),
-              GestureDetector(
-                onTap: () {
-                  launch("tel:+27 73 016 9813");
-                },
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text("Tel: ",
-                        style: GoogleFonts.nunito(
-                            fontSize: 18,
-                            fontWeight: FontWeight.w400,
-                            color: Colors.grey[800])),
-                    Text("073 016 9813",
-                        style: GoogleFonts.nunito(
-                          fontSize: 18,
-                          fontWeight: FontWeight.w400,
-                          color: Colors.grey[800],
-                          decoration: TextDecoration.underline,
-                          decorationColor: Colors.grey[800],
-                          decorationThickness: 2,
-                        )),
-                    Icon(
-                      Icons.call,
-                      color: Color(0xFF003049),
-                      // size: 30,
-                    ),
-                  ],
-                ),
-              ),
             ],
           ),
+          Image.asset("assets/images/skyline_2.png"),
           Container(child: ContactForm())
         ],
       ),
